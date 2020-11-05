c44db6f10d8c78139fe8d02c0b6d13bb0202d658
poplark <songkang666@126.com>
2020-11-05 22:20:15 +0800

    docs: 新增文档

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
+/**
+ * 版本号
+ */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 const version: string = __VERSION__;
 
 export { version };
 
+/**
+ * classNames 方法参数的基本类型 - string，number, object
+ */
 type BasicClassNameType = string | number | Record<string, any>;
-type FunctionClassNameType = () => BasicClassNameType;
+/**
+ * classNames 方法参数的特殊类型 - function，classNames 执行时，会先执行该传入的函数
+ */
+type FunctionClassNameType = () => any;
+/**
+ * classNames 方法参数的复合类型
+ */
 type ClassNameType<T extends BasicClassNameType | FunctionClassNameType> = T | T[];
 
 /**
@@ -43,17 +55,20 @@ function stringParser(clz: string): string[] {
 function objectParser(clz: Record<string, any>): Map<string, boolean> {
   const clzMap = new Map<string, boolean>();
   Object.keys(clz).forEach((key) => {
-    const val = !!clz[key];
+    const flag = !!clz[key];
     stringParser(key).forEach((item) => {
-      clzMap.set(item, val);
+      clzMap.set(item, flag);
     });
   });
   return clzMap;
 }
 
+/**
+ * 将多个 className 进行合并，生成不会重复的 className
+ * @param args - 可传入多个且不同类型的 className
+ */
 export function classNames<T>(...args: ClassNameType<T>[]): string {
   const clzMap = new Map<string, boolean>();
-  // 铺平
   args.forEach((item) => {
     switch (type(item)) {
       case 'String':
